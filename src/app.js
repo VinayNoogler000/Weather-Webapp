1f2b6b9db9cfcf05930211f7ad67b794b6272c20
VinayNoogler000 <vinaytambey000@gmail.com>
2025-05-20 21:38:58 +0530

    Refactor weather data fetching functions, improve error logging, and add function to render user-entered location on the webapp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import { config } from "../config.js";
-const API_URL = config.API_ENDPOINT; //Open-Weather-Map API URL
 const API_KEY = config.API_KEY; //Open-Weather-Map API Key 
 
 // Select the required DOM elements:
@@ -29,12 +28,32 @@ async function getGeoCoordinates(location) {
         return data[0];
     }
     catch (error) {
-        console.log(error);
+        console.log("Error in fetching the co-ordinates of the user-entered location.\n", error);
     }
 }
 
+// Function to get weather data by the co-ordinates of the location, entered by the user:
+async function getWeatherData(lat, lon) {
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather?"; //Open-Weather-Map API URL
+    try {
+        let response = await fetch(API_URL + `lat=${lat}&lon=${lon}&units=metric&lang=en&appid=${API_KEY}`);
+        let data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log("Error in fetching weather data of the location, by its coordinates.\n", error);
+    }
+}
+
+// Function to render the weather-data of the user-entered location:
+function renderWeatherData(name, state) {
+    // select location name element and replace it's content with the new location name:
+    const locationEl = document.querySelector(".location");
+    locationEl.textContent = `${name}, ${state}`;
+}
+
 // Function to get weather data of the user-entered location:
-function getWeatherData(event) {
+async function handleLocationWeatherSearch(event) {
     // prevent the default behaviour of form element:
     event.preventDefault();
 
@@ -44,8 +63,17 @@ function getWeatherData(event) {
     // get the co-ordinates of the location:
     getGeoCoordinates(location)
         .then( data => {
-            // print the fetched co-ordinates of the location:
-            console.log(data.name, data.state, data.lat, data.lon);
+            // get the name and state of the location to display in the "main-weather-card"
+            let {name, state, lat, lon} = data; 
+            
+            // update the name and state of location in the "main-weather" card
+            renderWeatherData(name, state);
+
+            // get the weather data by the co-ordinates of the location:
+            // return getWeatherData(lat, lon);
+        })
+        .then( data => {
+            console.log(data);
         })
         .catch(error => console.log(error));
 }
@@ -55,4 +83,4 @@ tempCelsiusBtn.addEventListener("click", () => toggleTemperatureUnits(tempCelsiu
 tempFahrenheitBtn.addEventListener("click", () => toggleTemperatureUnits(tempFahrenheitBtn));
 
 // Send a request to open-weather-map API
-searchForm.addEventListener("submit", getWeatherData)
+searchForm.addEventListener("submit", handleLocationWeatherSearch)
